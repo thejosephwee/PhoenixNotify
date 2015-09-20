94129f7ef541bb6eac5859be8c84a2ed067f8c80
thejosephwee <thejosephwee@gmail.com>
2015-09-21 07:22:40 +1000

    Added in some basic tiles to show output

--- scripts/task.js
+++ scripts/task.js
@@ -1,5 +1,6 @@
 'use strict';
 
 app.controller('TaskController', function($scope, $routeParams){
-
+    $scope.criticalBugs = [];
+    $scope.criticalBugsCount = $scope.criticalBugs.length | 2;
 });
