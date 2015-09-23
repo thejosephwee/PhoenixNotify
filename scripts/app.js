5ee113018c522d3c8496f45f75721fce9c904b81
Joseph Wee <Joseph.Wee@globalx.com.au>
2015-09-23 13:17:28 +1000

    Using bootstrap 4.0 Alpha

--- scripts/app.js
+++ scripts/app.js
@@ -18,3 +18,80 @@ var app = angular
                 redirectTo: '/'
             });
     });
+
+app.directive('chart', function() {
+    
+    return {
+        restrict: 'E',
+        replace: true,
+        scope: {
+            data: '=data'
+        },
+        template: '<div class="chart"></div>',
+        link: function(scope, element, attrs) {
+            
+            var chart = new google.visualization.LineChart(element[0]);
+            var options = {};
+            
+            scope.$watch('data', function(v) {
+                
+                var data = google.visualization.arrayToDataTable(v);
+                chart.draw(data, options);
+                
+            });
+            
+        }
+    };
+    
+});
+
+app.controller('ChartController', function($scope) {
+    
+    $scope.scoreHistory = [];
+    $scope.loadDataFromServer = function() {
+        
+        var x = [
+            ['interval', 'count']
+        ];
+        
+        var scoreHistory = [
+            {
+                intervalStart: 12,
+                count: 20
+            },
+            {
+                intervalStart: 100,
+                count: 200
+            },
+            {
+                intervalStart: 200,
+                count: 50
+            },
+            {
+                intervalStart: 250,
+                count: 150
+            }
+        ];
+        
+        angular.forEach(scoreHistory, function(record, key) {
+            
+            x.push([
+                record.intervalStart,
+                record.count
+            ]);
+            
+        });
+        
+        $scope.scoreHistory = x;
+        
+    };
+    
+});
+
+google.setOnLoadCallback(function() {
+    
+    angular.bootstrap(document, ['app']);
+    
+});
+
+google.load('visualization', '1', {packages: ['corechart']});
